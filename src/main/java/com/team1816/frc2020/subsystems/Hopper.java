7752725c7d85bb50b80f81c1118fd585c345eaad
Richik SC <sc.richik@gmail.com>
2020-01-21 18:26:35 -0600

    Frame out basic functionality of Hopper subsystem

--- src/main/java/com/team1816/frc2020/subsystems/Hopper.java
+++ src/main/java/com/team1816/frc2020/subsystems/Hopper.java
@@ -1,6 +1,10 @@
 package com.team1816.frc2020.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.IMotorControllerEnhanced;
+import com.team1816.frc2020.Robot;
 import com.team1816.lib.subsystems.Subsystem;
+import edu.wpi.first.wpilibj.Solenoid;
 
 public class Hopper extends Subsystem {
     private static final String NAME = "hopper";
@@ -14,13 +18,37 @@ public class Hopper extends Subsystem {
         return INSTANCE;
     }
 
-    private Hopper(){
+    // Components
+    private final Solenoid hopperSolenoid;
+    private final IMotorControllerEnhanced spindexer;
+
+    // State
+    private boolean hopperOut;
+    private double spindexerVelocity;
+    private boolean outputsChanged;
+
+    private Hopper() {
         super(NAME);
+        this.hopperSolenoid = Robot.getFactory().getSolenoid(NAME, "hopper");
+        this.spindexer = Robot.getFactory().getMotor(NAME, "solenoid");
+    }
+
+    public void setHopperPivot(boolean hopperOut) {
+        this.hopperOut = hopperOut;
+        outputsChanged = true;
+    }
+
+    public void setSpindexer(double spindexerOutput) {
+        this.spindexerVelocity = spindexerOutput;
+        outputsChanged = true;
     }
 
     @Override
     public void writePeriodicOutputs() {
-
+        if (outputsChanged) {
+            this.spindexer.set(ControlMode.PercentOutput, spindexerVelocity);
+            this.hopperSolenoid.set(hopperOut);
+        }
     }
 
     @Override
