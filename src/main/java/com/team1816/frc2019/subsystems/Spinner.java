8d6913be4e9363ab43f965a3a906cad6b70f911c
Richik SC <sc.richik@gmail.com>
2020-01-31 21:10:35 -0600

    Refactor Spinner and determineColor to use enum

--- src/main/java/com/team1816/frc2019/subsystems/Spinner.java
+++ src/main/java/com/team1816/frc2019/subsystems/Spinner.java
@@ -24,22 +24,33 @@ public class Spinner extends Subsystem {
     // Components
     private final IMotorControllerEnhanced spinnerMotor;
     private final I2C.Port i2cPort = I2C.Port.kOnboard;
-    private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
+    private final ColorSensorV3 colorSensor = new ColorSensorV3(i2cPort);
 
     // State
     private double spinnerPow;
     private boolean outputsChanged = false;
-    private int color = 0;
+    private SpinnerColor currentColor;
     private int colorsPassed = 0;
-    private boolean colorChanged = false;
 
     // Constants
-    //                           R    G   B
-    private final double[] Blue = {138, 290, 627};
-    private final double[] Green = {201, 396, 350};
-    private final double[] Red = {702, 220, 155};
-    private final double[] Yellow = {403, 390, 144};
-    private final double[][] colors = {Blue, Green, Red, Yellow};
+    public enum SpinnerColor {
+        BLUE(138, 290, 627, 0),
+        GREEN(201, 396, 350, 1),
+        RED(702, 220, 155, 2),
+        YELLOW(403, 390, 144, 3);
+
+        public final int red;
+        public final int green;
+        public final int blue;
+        public final int index;
+
+        SpinnerColor(int red, int green, int blue, int index) {
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+            this.index = index;
+        }
+    }
 
     private Spinner() {
         super(NAME);
@@ -48,7 +59,7 @@ public class Spinner extends Subsystem {
     }
 
     public void initialize() {
-        color = detetermineColor(m_colorSensor, colors);
+        currentColor = determineColor();
     }
 
     public void setSpinnerPower(double power) {
@@ -56,52 +67,51 @@ public class Spinner extends Subsystem {
         outputsChanged = true;
     }
 
+    @Override
+    public void readPeriodicInputs() {
+        SpinnerColor detectedColor = determineColor();
+        if (detectedColor != currentColor) {
+            if (detectedColor.index - currentColor.index == 1 || currentColor.index - detectedColor.index == 3) {
+                colorsPassed++;
+                currentColor = detectedColor;
+            }
+
+        }
+        System.out.println(colorsPassed);
+    }
+
     public void writePeriodicOutputs() {
         if (outputsChanged) {
             spinnerMotor.set(ControlMode.PercentOutput, spinnerPow);
             outputsChanged = false;
         }
-        int detectedColor = detetermineColor(m_colorSensor, colors);
-        if (detectedColor != color) {
-            if (detectedColor - color == 1 || color - detectedColor == 3) {
-                colorsPassed++;
-                color = detectedColor;
-            }
-
-        }
-        System.out.println(colorsPassed);
-
         //System.out.println("D: "+IR);
     }
 
-    private static int detetermineColor(ColorSensorV3 m_colorSensor, double[][] colors) {
-        Color detectedColor = m_colorSensor.getColor();
-        double IR = m_colorSensor.getIR();
-        double redOut = detectedColor.red * 1247;
-        double greenOut = detectedColor.green * 685;
-        double blueOut = detectedColor.blue * 1354;
-        int colorDetected = 0;
-        double error = Integer.MAX_VALUE;
-        for (int i = 0; i < 4; i++) {
-            double[] color = colors[i];
-            double errorTemp = Math.abs(redOut - color[0]) / color[0] + Math.abs(greenOut - color[1]) / color[1] + Math.abs(blueOut - color[2]) / color[2];
-            if (error > errorTemp) {
-                error = errorTemp;
-                colorDetected = i;
+    private SpinnerColor determineColor() {
+        // TODO: Evaluate using built-in REV ColorMatch class?
+        Color sensorColor = colorSensor.getColor();
+        double redOut = sensorColor.red * 1247;
+        double greenOut = sensorColor.green * 685;
+        double blueOut = sensorColor.blue * 1354;
+
+        SpinnerColor colorDetected = SpinnerColor.BLUE;
+        double minError = Integer.MAX_VALUE;
+        for (SpinnerColor color : SpinnerColor.values()) {
+            double error =
+                (Math.abs(redOut - color.red) / color.red)
+                    + (Math.abs(greenOut - color.green) / color.green)
+                    + (Math.abs(blueOut - color.blue) / color.blue);
+            if (error < minError) {
+                minError = error;
+                colorDetected = color;
             }
         }
-        if (colorDetected == 0) {
-            System.out.println("Blue");
-        } else if (colorDetected == 1) {
-            System.out.println("Green");
-        } else if (colorDetected == 2) {
-            System.out.println("Red");
-        } else if (colorDetected == 3) {
-            System.out.println("Yellow");
-        }
-        System.out.print("R: " + detectedColor.red * 1.247);
-        System.out.print("G: " + detectedColor.green * 0.685);
-        System.out.println("B: " + detectedColor.blue * 1.354);
+
+        System.out.println(colorDetected);
+        System.out.print("R: " + sensorColor.red * 1.247);
+        System.out.print("G: " + sensorColor.green * 0.685);
+        System.out.println("B: " + sensorColor.blue * 1.354);
         return colorDetected;
     }
 
