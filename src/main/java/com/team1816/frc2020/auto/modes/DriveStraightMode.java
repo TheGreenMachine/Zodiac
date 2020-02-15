8b8f24e3bcde354de358f91c32dd3e0a8e2420f2
Richik SC <sc.richik@gmail.com>
2020-02-14 19:30:16 -0600

    Reenable hopper and shooter

--- src/main/java/com/team1816/frc2020/auto/modes/DriveStraightMode.java
+++ src/main/java/com/team1816/frc2020/auto/modes/DriveStraightMode.java
@@ -17,7 +17,7 @@ public class DriveStraightMode extends AutoModeBase {
 
     @Override
     protected void routine() throws AutoModeEndedException {
-        System.out.println("Running Drive Straight Mode");;
+        System.out.println("Running Drive Straight Mode");
         runAction(new WaitAction(.5));
         runAction(mDriveStraight);
     }
