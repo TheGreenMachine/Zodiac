2a74d984a86154f605890ce4177e60002e0f473d
Richik SC <sc.richik@gmail.com>
2020-02-17 08:58:12 -0600

    Refactor CtreMotorFactory to use LazyTalons

--- src/main/java/com/team1816/lib/hardware/CtreMotorFactory.java
+++ src/main/java/com/team1816/lib/hardware/CtreMotorFactory.java
@@ -3,9 +3,8 @@ package com.team1816.lib.hardware;
 import com.ctre.phoenix.ErrorCode;
 import com.ctre.phoenix.ParamEnum;
 import com.ctre.phoenix.motorcontrol.*;
-import com.ctre.phoenix.motorcontrol.can.BaseMotorController;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.ctre.phoenix.motorcontrol.can.*;
+import com.team254.lib.drivers.LazyTalonSRX;
 
 /**
  * A class to create Falcon (TalonFX), TalonSRX, VictorSPX, and GhostTalonSRX objects.
@@ -73,12 +72,19 @@ public class CtreMotorFactory {
     }
 
     private static IMotorControllerEnhanced createTalon(int id, Configuration config, boolean isFalcon) {
-        TalonSRX talon = new TalonSRX(id);
+        BaseTalon talon = isFalcon ? new LazyTalonFX(id) : new LazyTalonSRX(id);
         configureMotorController(talon, config);
 
         talon.configReverseLimitSwitchSource(LimitSwitchSource.FeedbackConnector,
                 LimitSwitchNormal.NormallyOpen, kTimeoutMs);
-        talon.enableCurrentLimit(config.ENABLE_CURRENT_LIMIT);
+
+        if (talon instanceof TalonSRX) {
+            ((TalonSRX) talon).enableCurrentLimit(config.ENABLE_CURRENT_LIMIT);
+        } else {
+            ((TalonFX) talon).configSupplyCurrentLimit(
+                new SupplyCurrentLimitConfiguration(config.ENABLE_CURRENT_LIMIT, 0, 0, 0)
+            );
+        }
 
         talon.setStatusFramePeriod(StatusFrameEnhanced.Status_1_General,
                 config.GENERAL_STATUS_FRAME_RATE_MS, kTimeoutMs);
