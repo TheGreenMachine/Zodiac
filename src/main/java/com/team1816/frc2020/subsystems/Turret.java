981882d6d3ebb9ee94740c3f051f5896fccabbe6
Richik SC <sc.richik@gmail.com>
2020-02-21 08:21:39 -0600

    Convert atan2 to degrees

--- src/main/java/com/team1816/frc2020/subsystems/Turret.java
+++ src/main/java/com/team1816/frc2020/subsystems/Turret.java
@@ -68,6 +68,7 @@ public class Turret extends Subsystem implements PidProvider {
 
         SmartDashboard.putNumber("TURRET_POSITION_MIN", TURRET_POSITION_MIN);
         SmartDashboard.putNumber("TURRET_POSITION_MAX", TURRET_POSITION_MAX);
+        SmartDashboard.putNumber("atan2 Vision", 0);
 
         this.kP = factory.getConstant(NAME, "kP");
         this.kI = factory.getConstant(NAME, "kI");
@@ -100,7 +101,8 @@ public class Turret extends Subsystem implements PidProvider {
             var deltaXPixels = (value.getDouble() - (VIDEO_WIDTH / 2)); // Calculate deltaX from center of screen
             this.deltaXAngle = deltaXPixels * (CAMERA_FOV / VIDEO_WIDTH) + VISION_HOMING_BIAS; // Multiply by FOV to pixel ratio
             // TODO: test this formula
-            SmartDashboard.putNumber("atan2 Vision", Math.atan2(deltaXPixels, CAMERA_FOCAL_LENGTH) + VISION_HOMING_BIAS);
+            SmartDashboard.getEntry("atan2 vision").setDouble(
+                Math.toDegrees(Math.atan2(deltaXPixels, CAMERA_FOCAL_LENGTH) + VISION_HOMING_BIAS));
         }, EntryListenerFlags.kNew | EntryListenerFlags.kUpdate);
     }
 
