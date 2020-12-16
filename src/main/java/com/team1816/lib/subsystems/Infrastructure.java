f1facd95c343deac5d2f7f2629a4ef6ad6d0f106
Richik SC <sc.richik@gmail.com>
2020-12-16 16:07:48 -0600

    Use ICompressor through RobotFactory and ghost solenoids when PCM is disabled

--- src/main/java/com/team1816/lib/subsystems/Infrastructure.java
+++ src/main/java/com/team1816/lib/subsystems/Infrastructure.java
@@ -2,6 +2,7 @@ package com.team1816.lib.subsystems;
 
 import com.team1816.frc2020.Constants;
 import com.team1816.frc2020.subsystems.Superstructure;
+import com.team1816.lib.hardware.components.pcm.ICompressor;
 import com.team1816.lib.loops.ILooper;
 import com.team1816.lib.loops.Loop;
 import edu.wpi.first.wpilibj.Compressor;
@@ -14,7 +15,7 @@ public class Infrastructure extends Subsystem {
     private static Infrastructure mInstance;
 
     private Superstructure mSuperstructure = Superstructure.getInstance();
-    private Compressor mCompressor;
+    private ICompressor mCompressor;
 
     private boolean mIsManualControl = false;
     private static final boolean COMPRESSOR_ENABLED = factory.getConstant("compressorEnabled") > 0;
@@ -23,7 +24,7 @@ public class Infrastructure extends Subsystem {
     private Infrastructure() {
         super("Infrastructure");
         if (Constants.kPCMId >= 0) {
-            mCompressor = new Compressor(Constants.kPCMId);
+            mCompressor = factory.getCompressor();
         }
     }
 
