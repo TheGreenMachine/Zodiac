0eb2b49637e177c5077a11f4f92fa9f09523f2bd
David Day <dcday@themilwaukeeroad.com>
2021-04-02 22:14:59 -0500

    Code to prevent unexpected robot launches
    - removed crash tracker
    - fixed double test runs
    - updated robot to display red leds if not initialized (if it can)
    - if robot is in faulted state it will not let any of the modes execute

--- src/main/java/com/team1816/lib/subsystems/SubsystemManager.java
+++ src/main/java/com/team1816/lib/subsystems/SubsystemManager.java
@@ -35,7 +35,7 @@ public class SubsystemManager implements ILooper {
         boolean ret_val = true;
 
         for (Subsystem s : mAllSubsystems) {
-            System.out.println("SUBSYSTEM: " + s.getName() + ": " + s.checkSystem());
+            System.out.println("SUBSYSTEM: " + s.getName());
             ret_val &= s.checkSystem();
         }
 
