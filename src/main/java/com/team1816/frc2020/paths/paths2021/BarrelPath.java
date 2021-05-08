e865370ac2d69b8b27f8051358189f304673ae42
Richik SC <sc.richik@gmail.com>
2021-05-08 00:04:51 -0500

    Restore Barrel Path

--- src/main/java/com/team1816/frc2020/paths/paths2021/BarrelPath.java
+++ src/main/java/com/team1816/frc2020/paths/paths2021/BarrelPath.java
@@ -15,20 +15,19 @@ public class BarrelPath implements PathContainer {
     @Override
     public List<Pose2d> buildWaypoints() {
         /*
-        List.of(
-            new Pose2d(0, 0, -90),
-            new Pose2d(50, -50, 0),
-            new Pose2d(100, 0, 90),
-            new Pose2d(50, 50, 180),
-            new Pose2d(0, 0, -90),
-            new Pose2d(50, -50, 0),
-            new Pose2d(100, 0, 90),
-            new Pose2d(50, 50, 180),
-            new Pose2d(0, 0, -90),
-            new Pose2d(50, -50, 0),
-            new Pose2d(100, 0, 90),
-            new Pose2d(50, 50, 180),
-            new Pose2d(0, 0, -90)
+        return List.of(
+            new Pose2d(39, 93, 0),
+            new Pose2d(150, 101, 0),
+            new Pose2d(149, 25, 180),
+            new Pose2d(143, 99, 10),
+            new Pose2d(266, 83, 30),
+            new Pose2d(248, 156, 176),
+            new Pose2d(208, 90, -60),
+            new Pose2d(275, 41, -25),
+            new Pose2d(326, 94, 134),
+            new Pose2d(208, 95, 180),
+            new Pose2d(143, 95, 180),
+            new Pose2d(39, 96, 180)
         );
          */
         return List.of(
