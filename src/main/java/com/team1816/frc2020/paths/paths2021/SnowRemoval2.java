db724d25e1d5db68f0c78c1649869089566dfe1f
Nico <nicholasl23638@gmail.com>
2021-05-15 14:51:01 -0500

    Snow Removal Path WORKING and competition ready

--- src/main/java/com/team1816/frc2020/paths/paths2021/SnowRemoval2.java
+++ src/main/java/com/team1816/frc2020/paths/paths2021/SnowRemoval2.java
@@ -17,7 +17,7 @@ public class SnowRemoval2 implements PathContainer {
         return List.of(
             new Pose2d(211, 151, -145),
             new Pose2d(129, 38, 180),
-            new Pose2d(91, 54, 135)
+            new Pose2d(91, 54, 125)
         );
     }
 
--- src/main/java/com/team1816/frc2020/paths/paths2021/SnowRemoval3.java
+++ src/main/java/com/team1816/frc2020/paths/paths2021/SnowRemoval3.java
@@ -18,14 +18,14 @@ public class SnowRemoval3 implements PathContainer {
             new Pose2d(90, 54, 0),
             new Pose2d(218, 56, 5),
             new Pose2d(300, 155, 0),
-            new Pose2d(322, 121, -90),
-            new Pose2d(254, 65, -90),
+            new Pose2d(325, 119, -90),
+            new Pose2d(249, 68, -51),
             new Pose2d(299, 39, 0),
-            new Pose2d(325, 60, 90),
-            new Pose2d(297, 83, 180),
+            new Pose2d(330, 60, 90),
+            new Pose2d(300, 96, 180),
             new Pose2d(211, 61, 180),
-            new Pose2d(84, 83, 180),
-            new Pose2d(30, 45, -100)
+            new Pose2d(80, 101, 180),
+            new Pose2d(20, 42, -100)
         );
     }
 
