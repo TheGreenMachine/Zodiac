ba62ce099e5f6e7bdef434333d428f90344558a5
Richik SC <sc.richik@gmail.com>
2021-08-20 13:01:23 -0500

    Make SeasonFactory getInstance consistent - late return, conditional init

--- src/main/java/com/team1816/frc2020/SeasonFactory.java
+++ src/main/java/com/team1816/frc2020/SeasonFactory.java
@@ -6,21 +6,21 @@ import com.team1816.frc2020.subsystems.SwerveDrive;
 import com.team1816.frc2020.subsystems.WestCoastDrive;
 import com.team1816.lib.hardware.RobotFactory;
 
-import javax.inject.Inject;
 @Singleton
 public class SeasonFactory implements Drive.Factory {
 
-    private RobotFactory factory = Robot.getFactory();
+    private final RobotFactory factory = Robot.getFactory();
     private static Drive mDrive;
+
     @Override
     public Drive getInstance() {
-        if(mDrive!=null)
-            return mDrive;
-        boolean isSwerve = factory.getConstant(Drive.NAME, "isSwerve") == 1;
-        if(isSwerve){
-            mDrive = new SwerveDrive();
-        } else {
-            mDrive = new WestCoastDrive();
+        if (mDrive == null) {
+            boolean isSwerve = factory.getConstant(Drive.NAME, "isSwerve") == 1;
+            if (isSwerve) {
+                mDrive = new SwerveDrive();
+            } else {
+                mDrive = new WestCoastDrive();
+            }
         }
         return mDrive;
     }
